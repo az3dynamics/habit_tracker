628b9f3f02ff1712656391e6b94a3f28d2b0a1fa
az3dynamics <az3dynamics@gmail.com>
2025-08-11 21:02:46 +0800

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:habit_tracker/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('HomeScreen UI Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the title is 'Habit Tracker'.
+    expect(find.text('Habit Tracker'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the FloatingActionButton with an add icon is present.
+    expect(find.byIcon(Icons.add), findsOneWidget);
   });
 }
